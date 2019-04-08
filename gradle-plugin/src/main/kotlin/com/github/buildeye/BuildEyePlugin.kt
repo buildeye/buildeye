26f6a86154a73e299f64cf651ba155b36b6b9474
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2019-04-09 00:56:30 +0300

    Fix plugin initialization
    There were two issues:
    1. BuildAdapter#buildStarted is called before a plugin is applied,
    so it is never called, resulting in the exception,
    because lateinit properties are not initialized.
    2. There will be a build listener per subproject, which may be excessive.
    
    After this change only one BuildListener instance is created.
    BuildListener is initialized in constructor.

--- gradle-plugin/src/main/kotlin/com/github/buildeye/BuildEyePlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/buildeye/BuildEyePlugin.kt
@@ -3,9 +3,25 @@ package com.github.buildeye
 import com.github.buildeye.listeners.BuildListener
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.invocation.Gradle
 
 class BuildEyePlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        target.gradle.addBuildListener(BuildListener())
+        GlobalState.init(target.gradle)
+    }
+}
+
+private object GlobalState {
+    @Volatile
+    private var initialized: Boolean = false
+
+    @Synchronized
+    fun init(gradle: Gradle) {
+        if (initialized) return
+
+        gradle.addBuildListener(BuildListener(gradle))
+
+        initialized = true
+        gradle.rootProject.logger.info("Initialized BuildEye plugin")
     }
 }
